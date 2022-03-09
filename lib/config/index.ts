926bac94e2021eb3ec481e83d8fb8660cfb2a240
Jefferson Calmon <jeffjeff0197@gmail.com>
2022-03-08 21:17:38 -0300

    :feat: feat: Add `PandoraConfig`

--- lib/config/index.ts
+++ lib/config/index.ts
@@ -46,3 +46,35 @@ export const config = (): void => {
         return pattern.replace(/#/g, () => value[index++] || '');
     };
 };
+
+export const PandoraConfig = (): React.ReactNode => {
+    Number.prototype.toMoney = function (
+        config: MoneyConfig = {
+            locale: 'pt-BR',
+            currency: 'BRL',
+        },
+    ) {
+        return this.toLocaleString(config.locale, {
+            style: 'currency',
+            currency: config.currency,
+            maximumFractionDigits: 2,
+        });
+    };
+
+    String.prototype.toNumber = function () {
+        return Number(this);
+    };
+
+    String.prototype.extractNumbers = function () {
+        return this.toString().replace(/\D/g, '');
+    };
+
+    String.prototype.mask = function (pattern: string) {
+        let index = 0;
+        const value = this.toString().replace(/\D/g, '');
+
+        return pattern.replace(/#/g, () => value[index++] || '');
+    };
+
+    return undefined;
+}
