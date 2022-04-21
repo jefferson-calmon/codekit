3aea59e87063106cc81de16bb1861f02ef39f04a
Jefferson Calmon <jeffjeff0197@gmail.com>
2022-04-21 15:00:58 -0300

    :rocket: chore(hooks): Add export to `useFetch`

--- lib/hooks/index.ts
+++ lib/hooks/index.ts
@@ -1,3 +1,4 @@
 export { usePersistedState } from './usePersistedState';
 export { useIsomorphicLayoutEffect } from './useIsomorphicLayoutEffect';
 export { useWindowSize } from './useWindowSize';
+export { useFetch } from './useFetch';
