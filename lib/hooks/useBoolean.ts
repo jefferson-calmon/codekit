c75cfee35baab40589c619be57879d94f3e8a658
Jefferson Calmon <jeffjeff0197@gmail.com>
2023-01-04 09:30:43 -0300

    :rocket: feat: Add hook `useBoolean`

---
+++ lib/hooks/useBoolean.ts
@@ -0,0 +1,21 @@
+import { Dispatch, SetStateAction, useCallback, useState } from 'react';
+
+interface HookReturn {
+    value: boolean;
+    setValue: Dispatch<SetStateAction<boolean>>;
+    setTrue: () => void;
+    setFalse: () => void;
+    toggle: () => void;
+}
+
+export function useBoolean(defaultValue?: boolean): HookReturn {
+    // States
+    const [value, setValue] = useState(!!defaultValue);
+
+    // Callbacks
+    const setTrue = useCallback(() => setValue(true), []);
+    const setFalse = useCallback(() => setValue(false), []);
+    const toggle = useCallback(() => setValue(x => !x), []);
+
+    return { value, setValue, setTrue, setFalse, toggle };
+}
