ce078a7a261cc6217dd4ca08e035b88e5708622f
Jefferson Calmon <jeffjeff0197@gmail.com>
2023-10-03 17:35:20 -0300

    :bug: fix(hooks/usePagintion): Resolve simples bugs in validations

--- lib/hooks/usePagination.ts
+++ lib/hooks/usePagination.ts
@@ -1,73 +1,76 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useState, useMemo } from 'react';
 
-export const usePagination = <T>(data: T[], pageSize: number) => {
-    // States
-    const [pageIndex, setPageIndex] = useState(0);
+export const usePagination = <T>(data: T[], pageSize = 6) => {
+	// States
+	const [pageIndex, setPageIndex] = useState(0);
 
-    // Memo vars
-    const pages = useMemo(() => {
-        const pages = [];
-        const pagesCount = Math.ceil(data.length / pageSize);
+	// Memo vars
+	const pages = useMemo(() => {
+		const pages = [];
+		const pagesCount = Math.ceil(data.length / pageSize);
 
-        for (let pageIndex = 1; pageIndex <= pagesCount; pageIndex++) {
-            const page: T[] = [];
-            const counter = pageIndex * pageSize - pageSize;
-            const limiter = counter + pageSize;
+		for (let pageIndex = 1; pageIndex <= pagesCount; pageIndex++) {
+			const page: T[] = [];
+			const counter = pageIndex * pageSize - pageSize;
+			const limiter = counter + pageSize;
 
-            for (let index = counter; index < limiter; index++) {
-                if (data[index]) {
-                    page.push(data[index]);
-                }
-            }
+			for (let index = counter; index < limiter; index++) {
+				if (data[index]) {
+					page.push(data[index]);
+				}
+			}
 
-            pages.push(page);
-        }
+			pages.push(page);
+		}
 
-        return pages;
-    }, []);
+		return pages;
+	}, [data, pageSize]);
 
-    const page = useMemo(() => {
-        const pageItems = pages[pageIndex];
-        if (!pageItems) return null;
+	const page = useMemo(() => {
+		const pageItems = pages[pageIndex];
 
-        return {
-            number: pageIndex + 1,
-            index: pageIndex,
-            items: pageItems,
-            canAdvance: pageIndex + 1 < pages.length,
-            canBack: pageIndex >= 1,
-        };
-    }, []);
+		return {
+			number: pageIndex + 1,
+			index: pageIndex,
+			items: pageItems,
+			incrementedItems: pages.slice(0, pageIndex + 1).flat(),
+			canAdvance: pageIndex + 1 < pages.length,
+			canBack: pageIndex >= 1,
+		};
+	}, [pageIndex, pages]);
 
-    const isFirstPage = useMemo(() => page?.index === 0, [page]);
-    const isLastPage = useMemo(() => page?.number === pages.length, [pages]);
+	const isFirstPage = useMemo(() => page.index === 0, [page]);
+	const isLastPage = useMemo(
+		() => page.number === pages.length,
+		[page, pages]
+	);
 
-    // Functions
-    function changePage(number: number) {
-        setPageIndex(number - 1);
-    }
+	// Functions
+	function changePage(number: number) {
+		setPageIndex(number - 1);
+	}
 
-    function nextPage() {
-        if (!isFirstPage) setPageIndex(prev => prev + 1);
-    }
+	function nextPage() {
+		if (!isLastPage) setPageIndex((prev) => prev + 1);
+	}
 
-    function prevPage() {
-        if (!isLastPage) setPageIndex(prev => prev - 1);
-    }
+	function prevPage() {
+		if (!isFirstPage) setPageIndex((prev) => prev - 1);
+	}
 
-    return {
-        pages,
-        page: {
-            ...page,
-            next: prevPage,
-            prev: nextPage,
-            changePage,
-        },
-        isFirstPage,
-        isLastPage,
-        prevPage,
-        nextPage,
-        changePage,
-    };
+	return {
+		pages,
+		page: {
+			...page,
+			next: prevPage,
+			prev: nextPage,
+			changePage,
+		},
+		isFirstPage,
+		isLastPage,
+		prevPage,
+		nextPage,
+		changePage,
+	};
 };
