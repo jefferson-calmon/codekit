b1964dc41808b10005969978ea3aef26002e6b56
Jefferson Calmon <jeffjeff0197@gmail.com>
2023-11-01 18:23:21 -0300

    :sparkles: feat(validators/cpf): Improve cpf validator adding the verification of last digits

--- lib/validators/validateCpf.ts
+++ lib/validators/validateCpf.ts
@@ -1,7 +1,24 @@
 export function validateCpf(cpf: string) {
-    const regex = /^\d{3}\.\d{3}\.\d{3}\-\d{2}$/;
+    cpf = cpf.replace(/\D/g, '');
 
-    const isValid = regex.test(cpf);
+    const cpfArray = cpf.split('').map(Number);
 
-    return isValid;
+    if (cpf.length !== 11) return false;
+    if (cpfArray.every(digit => digit === cpfArray[0])) return false;
+
+    // Calcula o primeiro dígito verificador
+    let sum = 0;
+    for (let i = 0; i < 9; i++) {
+        sum += cpfArray[i] * (10 - i);
+    }
+    const firstDigit = (sum * 10) % 11;
+
+    // Calcula o segundo dígito verificador
+    sum = 0;
+    for (let i = 0; i < 10; i++) {
+        sum += cpfArray[i] * (11 - i);
+    }
+    const secondDigit = (sum * 10) % 11;
+
+    return cpfArray[9] === firstDigit && cpfArray[10] === secondDigit;
 }
