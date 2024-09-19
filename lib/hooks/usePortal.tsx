7f720bb69c76c69151d2d9993d76081e79ab27c7
Jefferson Calmon <jeffjeff0197@gmail.com>
2024-09-19 13:38:25 -0300

    :sparkles: feat(hooks/usePortal): Introduce `usePortal` hook for rendering components in a portal.

---
+++ lib/hooks/usePortal.tsx
@@ -0,0 +1,62 @@
+import { useEffect, useState, useCallback, useMemo } from 'react';
+
+import { createPortal } from 'react-dom';
+
+interface MountedPortalProps {
+	children: Parameters<typeof createPortal>['0'];
+	container: HTMLElement;
+}
+
+type PortalProps = Pick<MountedPortalProps, 'children'>;
+type Container = HTMLElement | (() => HTMLElement | null) | null;
+
+export function usePortal(container: Container, waitMount = true) {
+	// Common vars
+	const containerElement =
+		typeof container === 'function' ? container() : container;
+
+	// Memo vars
+	const rootElement = useMemo(() => {
+		const isBrowser = typeof document !== 'undefined';
+
+		return isBrowser ? containerElement || document.body : null;
+	}, [containerElement]);
+
+	// Callbacks
+	const Portal = useCallback(
+		(props: PortalProps) => {
+			if (!rootElement) return null;
+
+			return createPortal(props.children, rootElement);
+		},
+		[rootElement]
+	);
+
+	const WaitMountPortal = useCallback(
+		(props: PortalProps) => {
+			if (!rootElement) return null;
+
+			return <MountedPortal container={rootElement} {...props} />;
+		},
+		[rootElement]
+	);
+
+	return {
+		target: rootElement,
+		Portal: waitMount ? WaitMountPortal : Portal,
+	};
+}
+
+function MountedPortal(props: MountedPortalProps) {
+    // States
+	const [mounted, setMounted] = useState(false);
+
+    // Effects
+	useEffect(() => setMounted(true), []);
+
+	if (props.container && mounted) {
+		return createPortal(props.children, props.container);
+	}
+
+	return null;
+}
