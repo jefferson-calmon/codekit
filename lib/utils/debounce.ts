09c081d25f16f69943579b9017f8191a23560b5f
Jefferson Calmon <jeffjeff0197@gmail.com>
2024-12-21 14:53:45 -0300

    :recycle: refactor(utils/debounce): Refactor `debounce` function to support cancellation and improve type safety.

--- lib/utils/debounce.ts
+++ lib/utils/debounce.ts
@@ -1,7 +1,22 @@
-let debounceTimeout: NodeJS.Timeout;
+export function debounce<T extends (...args: any[]) => void>(
+	func: T,
+	timeout: number = 1000,
+): { (...args: Parameters<T>): void; cancel: () => void } {
+	let timeoutId: NodeJS.Timeout | undefined;
 
-export function debounce(cb: () => void, timeout = 1000) {
-    clearTimeout(debounceTimeout);
+	const debounced = function (this: any, ...args: Parameters<T>) {
+		if (timeoutId) {
+			clearTimeout(timeoutId);
+		}
+		timeoutId = setTimeout(() => func.apply(this, args), timeout);
+	};
 
-    debounceTimeout = setTimeout(cb, timeout);
+	debounced.cancel = () => {
+		if (timeoutId) {
+			clearTimeout(timeoutId);
+			timeoutId = undefined;
+		}
+	};
+
+	return debounced;
 }
