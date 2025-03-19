9ab626738b9cc917ca65237adb9aa47ac30c9207
Jefferson Calmon <jeffjeff0197@gmail.com>
2025-03-19 13:25:48 -0300

    ✨ feat(utils/nanoid): Add `charset` parameter to `nanoid` and `customNanoId` for customizable ID generation

--- lib/utils/nanoId.ts
+++ lib/utils/nanoId.ts
@@ -3,14 +3,14 @@ import seedrandom from 'seedrandom';
 const urlAlphabet =
     'useandom-26T198340PX75pxJACKVERYMINDBUSHWOLF_GQZbfghjklqvwyzrict';
 
-export function nanoid(size = 21, seed?: string) {
+export function nanoid(size = 21, seed?: string, charset = urlAlphabet) {
     const rng = seed ? seedrandom(seed) : Math.random;
 
     let id = '';
     let i = size;
 
     while (i--) {
-        id += urlAlphabet[(rng() * 64) | 0];
+        id += charset[(rng() * 64) | 0];
     }
 
     return id;
@@ -20,11 +20,39 @@ export interface CustomNanoIdProps {
     prefix?: string;
     suffix?: string;
     size?: number;
+    charset?:
+        | string
+        | {
+              numbers: boolean;
+              lowercase: boolean;
+              uppercase: boolean;
+              underscore: boolean;
+              hyphen: boolean;
+              special: boolean;
+          };
 }
 
 export function customNanoId(props: CustomNanoIdProps) {
+    const charset =
+        typeof props.charset === 'string'
+            ? props.charset
+            : [
+                  props.charset?.numbers && '0123456789',
+                  props.charset?.lowercase && 'abcdefghijklmnopqrstuvwxyz',
+                  props.charset?.uppercase && 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+                  props.charset?.underscore && '_',
+                  props.charset?.hyphen && '-',
+                  props.charset?.special && '!@#$%^&*()_+-=[]{}|;:,.<>?',
+              ]
+                  .compact()
+                  .join('');
+
     return (seed?: string) => {
-        return [props.prefix, nanoid(props.size, seed), props.suffix]
+        return [
+            props.prefix,
+            nanoid(props.size, seed, props.charset ? charset : urlAlphabet),
+            props.suffix,
+        ]
             .compact()
             .uniq()
             .join('');
