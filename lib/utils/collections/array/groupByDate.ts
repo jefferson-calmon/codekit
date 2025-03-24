3f123de5b10bbfd10e16d2f358a6d306852ddb06
Jefferson Calmon <jeffjeff0197@gmail.com>
2025-03-23 21:56:57 -0300

    🌟 feat(utils/collections/array): Add timezone support to `groupByDate` function and update version to 2.23.11

--- lib/utils/collections/array/groupByDate.ts
+++ lib/utils/collections/array/groupByDate.ts
@@ -1,17 +1,23 @@
 import { group, GroupOptions } from './group';
 import { KeyOf } from '../../../types';
-
 export type DateUnit = 'minute' | 'hour' | 'day' | 'week' | 'month' | 'year';
+
+export interface GroupOptionsWithTimezone<K> extends GroupOptions<K> {
+    timezone?: string;
+}
 
 export function groupByDate<T extends object>(
     data: T[],
     key: KeyOf<T>,
     unit: DateUnit,
-    options?: GroupOptions<KeyOf<T>>,
+    options?: GroupOptionsWithTimezone<KeyOf<T>>,
 ) {
+    const timezone = options?.timezone || 'America/Sao_Paulo';
+
     const dateTransformer = (dateString: string): string => {
         const date = new Date(dateString);
-        const startDate = startOf(date, unit);
+        const zonedDate = new Date(date.toLocaleString('en-US', { timeZone: timezone }));
+        const startDate = startOf(zonedDate, unit);
         return startDate.toISOString();
     };
 
