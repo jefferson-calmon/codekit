cecc00d6d8fd6acff812ff7a537d3d4de034a05f
Jefferson Calmon <jeffjeff0197@gmail.com>
2026-05-09 02:33:04 -0300

    feat(utils/fetch): Add `params` property to `CreateInstanceProps` and improve `request` function to handle query parameters more effectively

--- lib/utils/fetch.ts
+++ lib/utils/fetch.ts
@@ -1,113 +1,127 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 export interface CreateInstanceProps {
-    baseUrl: string;
-    onBeforeRequest?: (
-        options: RequestInit,
-    ) => Promise<Partial<RequestInit>> | RequestInit;
-    onResponse?: (
-        response: Response,
-        refetch: () => Promise<Response>,
-    ) => Promise<Response> | Response;
-    headers?: RequestInit['headers'];
+	baseUrl: string;
+	headers?: RequestInit['headers'];
+	params?: Record<string, any>;
+
+	onBeforeRequest?: (
+		options: RequestInit,
+	) => Promise<Partial<RequestInit>> | RequestInit;
+	onResponse?: (
+		response: Response,
+		refetch: () => Promise<Response>,
+	) => Promise<Response> | Response;
 }
 
 export interface RequestOptions extends Omit<RequestInit, 'method' | 'body'> {
-    params?: Record<string, any>;
+	params?: Record<string, any>;
 }
 
-const url = (...paths: string[]) => paths.join('/').replaceAll('//', '/');
+const url = (...paths: string[]) =>
+	paths
+		.filter(Boolean)
+		.join('/')
+		.replace(/([^:]\/)\/+/g, '$1');
+
 const error = (data: any) => data?.message ?? data.error ?? data.detail;
 
 export function createFetchInstance(props: CreateInstanceProps) {
-    async function get<T>(endpoint: string, options?: RequestOptions) {
-        return await request<T>(endpoint, {
-            method: 'GET',
-            ...(options ?? {}),
-        });
-    }
+	async function get<T>(endpoint: string, options?: RequestOptions) {
+		return await request<T>(endpoint, {
+			method: 'GET',
+			...(options ?? {}),
+		});
+	}
 
-    async function post<T>(
-        endpoint: string,
-        data: any,
-        options?: RequestOptions,
-    ) {
-        return await request<T>(endpoint, {
-            method: 'POST',
-            body: JSON.stringify(data),
-            ...(options ?? {}),
-        });
-    }
+	async function post<T>(
+		endpoint: string,
+		data: any,
+		options?: RequestOptions,
+	) {
+		return await request<T>(endpoint, {
+			method: 'POST',
+			body: JSON.stringify(data),
+			...(options ?? {}),
+		});
+	}
 
-    async function del<T>(
-        endpoint: string,
-        data: any,
-        options?: RequestOptions,
-    ) {
-        return await request<T>(endpoint, {
-            method: 'DELETE',
-            body: JSON.stringify(data),
-            ...(options ?? {}),
-        });
-    }
+	async function del<T>(
+		endpoint: string,
+		data?: any,
+		options?: RequestOptions,
+	) {
+		return await request<T>(endpoint, {
+			method: 'DELETE',
+			body: data ? JSON.stringify(data) : undefined,
+			...(options ?? {}),
+		});
+	}
 
-    async function put<T>(
-        endpoint: string,
-        data: any,
-        options?: RequestOptions,
-    ) {
-        return await request<T>(endpoint, {
-            method: 'PUT',
-            body: JSON.stringify(data),
-            ...(options ?? {}),
-        });
-    }
+	async function put<T>(
+		endpoint: string,
+		data: any,
+		options?: RequestOptions,
+	) {
+		return await request<T>(endpoint, {
+			method: 'PUT',
+			body: JSON.stringify(data),
+			...(options ?? {}),
+		});
+	}
 
-    async function patch<T>(
-        endpoint: string,
-        data: any,
-        options?: RequestOptions,
-    ) {
-        return await request<T>(endpoint, {
-            method: 'PATCH',
-            body: JSON.stringify(data),
-            ...(options ?? {}),
-        });
-    }
+	async function patch<T>(
+		endpoint: string,
+		data: any,
+		options?: RequestOptions,
+	) {
+		return await request<T>(endpoint, {
+			method: 'PATCH',
+			body: JSON.stringify(data),
+			...(options ?? {}),
+		});
+	}
 
-    async function request<T>(
-        endpoint: string,
-        options: RequestInit & RequestOptions,
-    ) {
-        const params = new URLSearchParams(options.params).toString();
+	async function request<T>(
+		endpoint: string,
+		options: RequestInit & RequestOptions,
+	) {
+		const params = {
+			...(props.params ?? {}),
+			...(options.params ?? {}),
+		};
 
-        const requestUrl = url(
-            props.baseUrl,
-            [endpoint, params].compact().join('?'),
-        );
+		const searchParams = new URLSearchParams(params).toString();
 
-        if (props.onBeforeRequest) {
-            options = await props.onBeforeRequest(options);
-        }
+		const requestUrl = url(
+			props.baseUrl,
+			endpoint,
+			searchParams ? `?${searchParams}` : '',
+		);
 
-        let response = await fetch(requestUrl, {
-            ...options,
-            headers: { ...(props.headers ?? {}), ...(options.headers ?? {}) },
-        });
+		if (props.onBeforeRequest) {
+			options = await props.onBeforeRequest(options);
+		}
 
-        if (props.onResponse) {
-            response = await props.onResponse(response, () =>
-                request(endpoint, options),
-            );
-        }
+		let response = await fetch(requestUrl, {
+			...options,
+			headers: { ...(props.headers ?? {}), ...(options.headers ?? {}) },
+		});
 
-        if (!response.ok) throw new Error(error(await response.json()));
+		if (props.onResponse) {
+			response = await props.onResponse(response, () =>
+				request(endpoint, options),
+			);
+		}
 
-        const data = (await response.json()) as T;
+		const data = (await response.json()) as T;
 
-        return {
-            ...response,
-            data,
-        };
-    }
+		if (!response.ok) throw new Error(error(data));
 
-    return { get, post, put, patch, delete: del };
+		return {
+			...response,
+			data,
+		};
+	}
+
+	return { get, post, put, patch, delete: del };
 }
